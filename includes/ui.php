ceec3a4b0b0179b8ceae6a74b028c12623e1aed6
Pijush Gupta <pijush@live.com>
2023-06-08 14:40:22 +0530

    adding all code related ui into separate file

--- includes/ui.php
+++ includes/ui.php
@@ -7,12 +7,22 @@ class Ui{
         if(!function_exists('wp_get_current_user')) { include(ABSPATH . "wp-includes/pluggable.php"); }
 
         if (current_user_can('manage_options')) {
+            /**
+             * calling addMenu Method 
+             */
 			add_action('admin_menu', array('RespacioHouzezImport\Ui', 'addMenu'));
             add_action('admin_enqueue_scripts', array('RespacioHouzezImport\Ui', 'addAssets'));
 		}
     }
 
+    /**
+     * Adding menu items
+     *
+     * @return void
+     */
     public static function addMenu(){
-        
+        add_menu_page(__(RHIMO_PLUGIN_NAME), __(RHIMO_PLUGIN_NAME), 'manage_options', 'respacio_houzez_import', 'respacio_import', '', 6);
+		add_submenu_page('respacio_houzez_import','Import','Import','manage_options', 'respacio_houzez_import', 'respacio_import');
+		add_submenu_page('respacio_houzez_import','Export','Export','manage_options', 'respacio_houzez_export', 'respacio_export');
     }
 }
