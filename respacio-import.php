632d19a7078fb257ae96982e376bd74f4812d248
Pijush Gupta <pijush@live.com>
2023-06-08 17:12:05 +0530

    added more comment

--- respacio-import.php
+++ respacio-import.php
@@ -22,6 +22,11 @@ use RespacioHouzezImport\ui;
 add_action('plugin_loaded',function(){
     $mfwp_prefix = 'mfwp_';
     $mfwp_plugin_name = 'Respacio Houzez Import';
+
+    /**
+     * always define your constant as early as possible
+     * so from a single point we can control dynamic variable data
+     */
     if(!defined('RHIMO_PLUGIN_NAME')) define("RHIMO_PLUGIN_NAME", "Respacio Houzez Import");
     if(!defined('RHIMO_THEME_TYPE')) define("RHIMO_THEME_TYPE", "1");;
     if(!defined('RHIMO_API_BASE_URL')) define('RHIMO_API_BASE_URL',"https://crm.respacio.com/ws/properties");
