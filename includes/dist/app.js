2450a61f9e5171db6293a256ff0dda2f230e47f0
Pijush Gupta <pijush@live.com>
2023-07-21 16:09:35 +0530

    saving

--- includes/dist/app.js
+++ includes/dist/app.js
@@ -17150,6 +17150,7 @@ __webpack_require__.r(__webpack_exports__);
     __expose();
     var isDownloadActive = (0,vue__WEBPACK_IMPORTED_MODULE_1__.ref)(false);
     var selectedFileType = (0,vue__WEBPACK_IMPORTED_MODULE_1__.ref)('File Type');
+    var xmllink = (0,vue__WEBPACK_IMPORTED_MODULE_1__.ref)('');
     /**
      * showing the notification
      * and starting download process
@@ -17206,8 +17207,7 @@ __webpack_require__.r(__webpack_exports__);
           link.download = 'export.xls';
           link.click();
         } else {
-          var _notificaiotn2 = (0,vue_toastification__WEBPACK_IMPORTED_MODULE_0__.useToast)();
-          _notificaiotn2(res);
+          xmllink.value = res;
         }
 
         /**
@@ -17219,11 +17219,22 @@ __webpack_require__.r(__webpack_exports__);
         return console.log(err);
       });
     }
+    var open = (0,vue__WEBPACK_IMPORTED_MODULE_1__.ref)(false);
+    function copyToClipBoard() {
+      navigator.clipboard.writeText(xmllink.value).then(function () {
+        alert("Text copied to clipboard!");
+      })["catch"](function (error) {
+        console.error("Failed to copy text: ", error);
+      });
+    }
     var __returned__ = {
       isDownloadActive: isDownloadActive,
       selectedFileType: selectedFileType,
+      xmllink: xmllink,
       showDownloadNotification: showDownloadNotification,
       prepareDownload: prepareDownload,
+      open: open,
+      copyToClipBoard: copyToClipBoard,
       get useToast() {
         return vue_toastification__WEBPACK_IMPORTED_MODULE_0__.useToast;
       },
@@ -17567,14 +17578,65 @@ var _hoisted_6 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementV
   value: "2"
 }, "Excel", -1 /* HOISTED */);
 var _hoisted_7 = [_hoisted_5, _hoisted_6];
+var _hoisted_8 = {
+  key: 0,
+  "class": "w-full h-screen absolute top-0 backdrop-blur"
+};
+var _hoisted_9 = {
+  "class": "fixed z-50 top-1/3 left-1/2 w-[350px] bg-white rounded p-2 -ml-[150px] border"
+};
+var _hoisted_10 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("path", {
+  "stroke-linecap": "round",
+  "stroke-linejoin": "round",
+  d: "M9.75 9.75l4.5 4.5m0-4.5l-4.5 4.5M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+}, null, -1 /* HOISTED */);
+var _hoisted_11 = [_hoisted_10];
+var _hoisted_12 = {
+  "class": "px-2 py-2 flex flex-row justify-center items-center"
+};
+var _hoisted_13 = {
+  "class": "p-2 bg-gray-200 rounded overflow-x-auto whitespace-nowrap mr-1"
+};
+var _hoisted_14 = /*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("svg", {
+  xmlns: "http://www.w3.org/2000/svg",
+  fill: "none",
+  viewBox: "0 0 24 24",
+  "stroke-width": "1.5",
+  stroke: "currentColor",
+  "class": "w-6 h-6"
+}, [/*#__PURE__*/(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("path", {
+  "stroke-linecap": "round",
+  "stroke-linejoin": "round",
+  d: "M9 12h3.75M9 15h3.75M9 18h3.75m3 .75H18a2.25 2.25 0 002.25-2.25V6.108c0-1.135-.845-2.098-1.976-2.192a48.424 48.424 0 00-1.123-.08m-5.801 0c-.065.21-.1.433-.1.664 0 .414.336.75.75.75h4.5a.75.75 0 00.75-.75 2.25 2.25 0 00-.1-.664m-5.8 0A2.251 2.251 0 0113.5 2.25H15c1.012 0 1.867.668 2.15 1.586m-5.8 0c-.376.023-.75.05-1.124.08C9.095 4.01 8.25 4.973 8.25 6.108V8.25m0 0H4.875c-.621 0-1.125.504-1.125 1.125v11.25c0 .621.504 1.125 1.125 1.125h9.75c.621 0 1.125-.504 1.125-1.125V9.375c0-.621-.504-1.125-1.125-1.125H8.25zM6.75 12h.008v.008H6.75V12zm0 3h.008v.008H6.75V15zm0 3h.008v.008H6.75V18z"
+})], -1 /* HOISTED */);
+var _hoisted_15 = [_hoisted_14];
 function render(_ctx, _cache, $props, $setup, $data, $options) {
-  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_1, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_2, [_hoisted_3, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_4, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.withDirectives)((0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("select", {
+  return (0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Fragment, null, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_1, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_2, [_hoisted_3, (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_4, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.withDirectives)((0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("select", {
     "class": "w-full",
     onChange: $setup.showDownloadNotification,
     "onUpdate:modelValue": _cache[0] || (_cache[0] = function ($event) {
       return $setup.selectedFileType = $event;
     })
-  }, _hoisted_7, 544 /* HYDRATE_EVENTS, NEED_PATCH */), [[vue__WEBPACK_IMPORTED_MODULE_0__.vModelSelect, $setup.selectedFileType]])])])]);
+  }, _hoisted_7, 544 /* HYDRATE_EVENTS, NEED_PATCH */), [[vue__WEBPACK_IMPORTED_MODULE_0__.vModelSelect, $setup.selectedFileType]])])])]), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("button", {
+    onClick: _cache[1] || (_cache[1] = function ($event) {
+      return $setup.open = true;
+    })
+  }, "Open Modal"), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" modal - Teleport to appent the modal before body ends "), ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createBlock)(vue__WEBPACK_IMPORTED_MODULE_0__.Teleport, {
+    to: "body"
+  }, [$setup.open ? ((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("div", _hoisted_8, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_9, [((0,vue__WEBPACK_IMPORTED_MODULE_0__.openBlock)(), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementBlock)("svg", {
+    onClick: _cache[2] || (_cache[2] = function ($event) {
+      return $setup.open = false;
+    }),
+    xmlns: "http://www.w3.org/2000/svg",
+    fill: "none",
+    viewBox: "0 0 24 24",
+    "stroke-width": "1.5",
+    stroke: "currentColor",
+    "class": "w-6 h-6 absolute -right-2 -top-2 bg-white rounded-full cursor-pointer shadow-lg"
+  }, _hoisted_11)), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", _hoisted_12, [(0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("p", _hoisted_13, (0,vue__WEBPACK_IMPORTED_MODULE_0__.toDisplayString)($setup.xmllink.value), 1 /* TEXT */), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createElementVNode)("div", {
+    "class": "border p-[8px] rounded cursor-pointer",
+    onClick: $setup.copyToClipBoard
+  }, _hoisted_15)])])])) : (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)("v-if", true)])), (0,vue__WEBPACK_IMPORTED_MODULE_0__.createCommentVNode)(" modal ends  ")], 64 /* STABLE_FRAGMENT */);
 }
 
 /***/ }),
